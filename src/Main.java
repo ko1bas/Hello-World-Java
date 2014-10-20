b5642ad482183527be702e2bcae5bab6972b1834
Kolbasov Pavel <kolllbas@gmail.com>
2014-10-20 12:41:57 +0400

    Format to Java Convention
    Format to Java Convention

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,16 @@
  * @author mkuser11
  *
  */
-public class Main
-{
-    
-    /**
-     * @param args
-     */
-    public static void main(String[] args)
-    {
-        // TODO Auto-generated method stub
-        System.out.println("Hello World!");
-    }
-    
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long time = System.currentTimeMillis();
+		System.out.println("Hello World!");
+		System.out.println("Time: " + (System.currentTimeMillis() - time));
+	}
+
 }
